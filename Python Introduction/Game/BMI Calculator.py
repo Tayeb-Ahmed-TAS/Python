a1192fc59f30b18d08d106bd7661c84539964b69
Tayeb Ahmed <atayeb.english939@gmail.com>
2023-01-13 15:10:55 +0600

    BMI Calculator

---
+++ Python Introduction/Game/BMI Calculator.py
@@ -0,0 +1,74 @@
+# BMI Calculator
+# ? Formula for kb/m, BMI = kg / m ^ 2
+# ? Formula for lbs/inch, BMI = (703 * lbs) / inch ^ 2
+
+
+def measurements():
+
+    print("1. Kilograms and meters\n2. Pounds and inches")
+
+    measurement = int(input("1 or 2 ? => "))
+
+    if measurement == 1:
+
+        kg_m()
+
+    elif measurement == 2:
+
+        lb_inch()
+
+    else:
+
+        print("Wrong Input ! Select again")
+
+        measurements()
+
+
+def kg_m():
+
+    weight = float(input("Enter weight in kg => "))
+
+    height = float(input("Enter height in meters => "))
+
+    bmi = weight / pow(height, 2)
+
+    BMI_ind(bmi)
+
+
+def lb_inch():
+
+    weight = float(input("Enter weight in lbs => "))
+
+    height = float(input("Enter height in inch => "))
+
+    bmi = (703 * weight) / pow(height, 2)
+
+    BMI_ind(bmi)
+
+
+def BMI_ind(bmi_val):
+
+    bmi_list = ["Under Weight", "Normal Weight", "Over Weight", "Obesity"]
+
+    bmi_rsl = "BMI retult : "
+
+    if bmi_val < 18.5:
+
+        print(bmi_rsl + bmi_list[0])
+
+    elif bmi_val >= 18.5 and bmi_val < 25:
+
+        print(bmi_rsl + bmi_list[1])
+
+    elif bmi_val >= 25 and bmi_val < 30:
+
+        print(bmi_rsl + bmi_list[2])
+
+    elif bmi_val >= 30:
+
+        print(bmi_rsl + bmi_list[3])
+
+
+print("\t\tBMI Calculator\n\nSelect Measurement Units")
+
+measurements()
