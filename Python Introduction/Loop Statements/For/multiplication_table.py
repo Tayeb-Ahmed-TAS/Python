7dc576b9f077f6d41c69f0ff508ce0b95ccb7e52
Tayeb Ahmed <atayeb.english939@gmail.com>
2023-03-24 17:12:26 +0600

    Python

--- Python Introduction/Loop Statements/For/multiplication_table.py
+++ Python Introduction/Loop Statements/For/multiplication_table.py
@@ -4,11 +4,11 @@
 # ? Enter the number => 4
 # ? Enter the row of the table => 5
 # ? Output:
-# ? 4 * 1 = 4
-# ? 4 * 2 = 8
-# ? 4 * 3 = 12
-# ? 4 * 4 = 16
-# ? 4 * 5 = 20
+# ? 4 x 1 = 4
+# ? 4 x 2 = 8
+# ? 4 x 3 = 12
+# ? 4 x 4 = 16
+# ? 4 x 5 = 20
 
 
 n = int(input("Enter the number => "))
@@ -16,4 +16,4 @@ n = int(input("Enter the number => "))
 row = int(input("Enter the row of the table => "))
 
 for i in range(1, row + 1):
-    print(f"{n} * {i} = {n*i}")
+    print(f"{n} x {i} = {n*i}")
